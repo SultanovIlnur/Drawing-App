2d759e24cf7c718d05590d9596d911de30c5bb78
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-09 09:21:54 +0300

    canvas init

---
+++ main.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('drawingCanvas');
+var canvasContext = canvas.getContex('2d');
+
+var fillStyle = 'black';
+var fillStyles = ['black', 'white', 'red', 'green', 'yellow'];
+var 
