9cf7d7711d5eb55568d45d5429b4b7105f526a82
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-11 15:04:30 +0300

    fixes

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-var canvas = document.getElementById('drawingCanvas');
-var canvasContext = canvas.getContex('2d');
-
 var fillStyle = 'black';
 var fillStyles = ['black', 'white', 'red', 'green', 'yellow'];
 
 var mode = 'paint';
 var modes = [];
+
+function getMousePosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+    canvas.addEventListener('mousemove', function (event) {
+        var mousePos = getMousePosition(canvas, event);
+    });
+}
+
