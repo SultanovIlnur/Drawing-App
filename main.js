6ca874ca1c59646c1b1f93bfd00736b50872c39b
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-11 23:07:57 +0300

    brush implemented

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
-var fillStyle = 'black';
-var fillStyles = ['black', 'white', 'red', 'green', 'yellow'];
+window.onload = function() {
+    var canvas = document.getElementById('drawingCanvas');
+    var context = canvas.getContext('2d');
+    var boundings = canvas.getBoundingClientRect();
 
-var mode = 'paint';
-var modes = [];
+    var operationButtons = document.getElementsByClassName('operationButtons')[0];
+    var currentOpButton = 0;
 
-function getMousePosition(canvas, event) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top
+    var fillStyle = 'black';
+    var fillStyles = ['black', 'white', 'red', 'green', 'yellow'];
+
+    var lineWidth = 1;
+
+    var mode = 'paint';
+
+    var mouse = {
+        X: 0,
+        Y: 0
     };
-    canvas.addEventListener('mousemove', function (event) {
-        var mousePos = getMousePosition(canvas, event);
-    });
-}
+    var isDrawing = false;
 
+    function setMousePosition(event) {
+        mouse.X = event.clientX - boundings.left;
+        mouse.Y = event.clientY - boundings.top;
+    }
+
+    var clearButton = document.getElementById('clearButton');
+
+    canvas.addEventListener('mousedown', function(event) {
+        isDrawing = true;
+        setMousePosition(event);
+        context.beginPath();
+        context.moveTo(mouse.X, mouse.Y);
+    });
+
+    canvas.addEventListener('mousemove', function(event) {
+        setMousePosition(event);
+        if (isDrawing) {
+            context.lineTo(mouse.X, mouse.Y);
+            context.stroke();
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(event) {
+        setMousePosition(event);
+        isDrawing = false;
+    });
+
+    canvas.addEventListener('', function() {
+
+    });
+};
