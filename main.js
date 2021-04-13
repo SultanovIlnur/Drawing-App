c83d5a912e5221ca2bafa8d2956744d41d9485a8
SultanovIlnur <68385484+SultanovIlnur@users.noreply.github.com>
2021-04-13 17:20:14 +0300

    Added colorpicker tool

--- main.js
+++ main.js
@@ -8,20 +8,25 @@ window.onload = function() {
 
     var modeButtons = document.getElementById('modeButtons');
 
-    const fillStyles = ['Black', 'White', 'Red', 'Green', 'Yellow'];
-    var fillStyle = fillStyles[0];
+    var color = "#000000";
 
     var lineWidth = 1;
 
     const currentModeText = document.getElementById('currentMode');
     currentModeText.innerHTML = "Paint";
     const currentMouseCoords = document.getElementById('currentMouseCoords');
+    const colorPicker = document.getElementById('colorPicker');
 
     var mouse = {
         X: 0,
         Y: 0
     };
     var isDrawing = false;
+
+    colorPicker.addEventListener("change", function(event){
+        color = event.target.value;
+        context.strokeStyle = color;
+    });
 
     function setMousePosition(event) {
         mouse.X = event.clientX - boundings.left;
@@ -32,7 +37,7 @@ window.onload = function() {
     var clearButton = document.getElementById('clearButton');
 
     canvas.addEventListener('mousedown', function(event) {
-        if (fillStyle == fillStyles[0]){
+        if (mode == modes[0]){
             isDrawing = true;
         setMousePosition(event);
         context.beginPath();
@@ -41,7 +46,7 @@ window.onload = function() {
     });
 
     canvas.addEventListener('mousemove', function(event) {
-        if (fillStyle == fillStyles[0]){
+        if (mode == modes[0]){
             setMousePosition(event);
             if (isDrawing) {
                 context.lineTo(mouse.X, mouse.Y);
@@ -51,7 +56,7 @@ window.onload = function() {
     });
 
     canvas.addEventListener('mouseup', function(event) {
-        if(fillStyle == fillStyles[0]){
+        if(mode == modes[0]){
             setMousePosition(event);
         isDrawing = false;
         }
@@ -60,21 +65,21 @@ window.onload = function() {
     modeButtons.addEventListener("click", function(event){
         const currentButton = event.target;
         switch (currentButton.innerHTML){
-            case fillStyles[0]:
+            case modes[0]:
                 currentModeText.innerHTML = currentButton.innerHTML;
-                fillStyle = fillStyles[0];
+                mode = modes[0];
                 break;
-            case fillStyles[1]:
+            case modes[1]:
                 currentModeText.innerHTML = currentButton.innerHTML;
-                fillStyle = fillStyles[1];
+                mode = modes[1];
                 break;
-            case fillStyles[2]:
+            case modes[2]:
                 currentModeText.innerHTML = currentButton.innerHTML;
-                fillStyle = fillStyles[2];
+                mode = modes[2];
                 break;
-            case fillStyles[3]:
+            case modes[3]:
                 currentModeText.innerHTML = currentButton.innerHTML;
-                fillStyle = fillStyles[3];
+                mode = modes[3];
                 break;
         }
     });
